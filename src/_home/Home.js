548f4854d8db19e79ea5d765b1eca52bd8274560
AmerBiro <amer.biro@gmail.com>
2022-11-01 14:25:43 +0000

    using useState

--- src/_home/Home.js
+++ src/_home/Home.js
@@ -1,11 +1,31 @@
+import { useState } from "react"
+
 const Home = () => {
-     
+     let [name, setName] = useState('Amer')
+
+     let [status, setStatus] = useState(false)
+     let [num, setNum] = useState()
+
      const clickButton = (e) => {
           console.log('Hello user', e)
      }
 
      const clickMeAgain = (name, e) => {
           console.log(name, e)
+     }
+
+     const changeName = (name) => {
+          setName('new name')
+     }
+
+     const changeStatus = (num) => {
+          if(status == false){
+               setNum(0)
+               setStatus(true)
+          }else{
+               setNum(1)
+               setStatus(false)
+          }
      }
 
      return ( 
@@ -16,6 +36,12 @@ const Home = () => {
                <button onClick={(e) => {
                     clickMeAgain('clicked agian', e.shiftKey)
                }}>click me again</button>
+
+               <p>{name}</p>
+               <button onClick={changeName}>change name</button>
+
+               <p>Status = {num}</p>
+               <button onClick={changeStatus}>change status</button>
 
           </div>
       );
