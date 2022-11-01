ba55a7658a344f79721cd7610afe76ea0a92118c
AmerBiro <amer.biro@gmail.com>
2022-11-01 12:33:01 +0000

    Adding styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
 import Navbar from './_navbar/Navbar';
 import Home from './_home/Home';
 
 function App() {
-  
   return (
+
     <div className="App">
       <Navbar/>
       <div className="content">
         <Home/>
       </div>
     </div>
+
   );
 }
 
