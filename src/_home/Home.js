c3a7a47d85a634b99782116d36f76d32bff0b4bb
AmerBiro <amer.biro@gmail.com>
2022-11-01 13:01:15 +0000

    adding events

--- src/_home/Home.js
+++ src/_home/Home.js
@@ -1,9 +1,22 @@
 const Home = () => {
      
+     const clickButton = (e) => {
+          console.log('Hello user', e)
+     }
+
+     const clickMeAgain = (name, e) => {
+          console.log(name, e)
+     }
+
      return ( 
           <div className="home">
                <h2>Home Page</h2>
-               <p>What to do next?</p>
+               <button onClick={clickButton}>Click me</button>
+
+               <button onClick={(e) => {
+                    clickMeAgain('clicked agian', e.shiftKey)
+               }}>click me again</button>
+
           </div>
       );
 }
--- src/_navbar/Navbar.js
+++ src/_navbar/Navbar.js
@@ -7,11 +7,7 @@ const Navbar = () => {
                <h1>Amer's app</h1>
                <div className="links">
                     <a href={home}>Home</a>
-                    <a href={create} style={{
-                         color: 'white',
-                         backgroundColor: '#f1356d',
-                         borderRadius: '8px'
-                    }}>New blog</a>
+                    <a href={create}>New blog</a>
                </div>
           </nav>
       );
