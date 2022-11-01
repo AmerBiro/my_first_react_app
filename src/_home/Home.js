a781eb826c3f0c78e5e8b0c6573122e4cd19e332
AmerBiro <amer.biro@gmail.com>
2022-11-01 15:15:30 +0000

    using mapping to output lists

--- src/_home/Home.js
+++ src/_home/Home.js
@@ -1,50 +1,70 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Home = () => {
-     let [name, setName] = useState('Amer')
+  {
+    /*   let [name, setName] = useState("Amer");
 
-     let [status, setStatus] = useState(false)
-     let [num, setNum] = useState()
+  let [status, setStatus] = useState(false);
+  let [num, setNum] = useState();
 
-     const clickButton = (e) => {
-          console.log('Hello user', e)
-     }
+  const clickButton = (e) => {
+    console.log("Hello user", e);
+  };
 
-     const clickMeAgain = (name, e) => {
-          console.log(name, e)
-     }
+  const clickMeAgain = (name, e) => {
+    console.log(name, e);
+  };
 
-     const changeName = (name) => {
-          setName('new name')
-     }
+  const changeName = (name) => {
+    setName("new name");
+  };
 
-     const changeStatus = (num) => {
-          if(status == false){
-               setNum(0)
-               setStatus(true)
-          }else{
-               setNum(1)
-               setStatus(false)
-          }
-     }
+  const changeStatus = (num) => {
+    if (status == false) {
+      setNum(0);
+      setStatus(true);
+    } else {
+      setNum(1);
+      setStatus(false);
+    }
+  }; */
+  }
 
-     return ( 
-          <div className="home">
-               <h2>Home Page</h2>
-               <button onClick={clickButton}>Click me</button>
+  const [blogs, setBlogs] = useState([
+     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
 
-               <button onClick={(e) => {
-                    clickMeAgain('clicked agian', e.shiftKey)
-               }}>click me again</button>
+  return (
+    <div className="home">
+      <h2>Home Page</h2>
+     
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>written by {blog.author}</p>
+        </div>
 
-               <p>{name}</p>
-               <button onClick={changeName}>change name</button>
+      ))}
 
-               <p>Status = {num}</p>
-               <button onClick={changeStatus}>change status</button>
+      {/*         <button onClick={clickButton}>Click me</button>
 
-          </div>
-      );
-}
- 
+      <button
+        onClick={(e) => {
+          clickMeAgain("clicked agian", e.shiftKey);
+        }}
+      >
+        click me again
+      </button>
+
+      <p>{name}</p>
+      <button onClick={changeName}>change name</button>
+
+      <p>Status = {num}</p>
+      <button onClick={changeStatus}>change status</button> */}
+    </div>
+  );
+};
+
 export default Home;
