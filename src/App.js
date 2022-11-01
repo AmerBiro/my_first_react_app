f8b9f0332b0081d9d9660e6a37ed8ca875e917ef
AmerBiro <amer.biro@gmail.com>
2022-11-01 11:34:03 +0000

    Adding more values

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 
 function App() {
+
+  const title = "Amer"
+  const age = 32
+  const arr = [1,2,3,4,5]
+  const person = {name: "Amer", age: 32}
+  const link = "https://www.google.com/"
+  
   return (
     <div className="App">
       <div className="content">
         <h1>
           App Component
         </h1>
+        <p>{title} {age} </p>
+        <p>{arr}</p>
+        <a href={link}>Google</a>
       </div>
     </div>
   );
