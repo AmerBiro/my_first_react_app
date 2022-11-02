98ddc07e34ddb8033889933401ec3fcedc6d5a2d
AmerBiro <amer.biro@gmail.com>
2022-11-02 18:51:42 +0000

    replacing herf with Link to

--- src/_navbar/Navbar.js
+++ src/_navbar/Navbar.js
@@ -1,16 +1,17 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
-     const home = "/"
-     const create = "/create"
-     
-     return ( 
-          <nav className="navbar">
-               <h1>Amer's app</h1>
-               <div className="links">
-                    <a href={home}>Home</a>
-                    <a href={create}>New blog</a>
-               </div>
-          </nav>
-      );
-}
- 
+  const home = "/";
+  const create = "/create";
+
+  return (
+    <nav className="navbar">
+      <h1>Amer's app</h1>
+      <div className="links">
+        <Link to={home}>Home</Link>
+        <Link to={create}>New blog</Link>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
