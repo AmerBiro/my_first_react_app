2c8e5339274fe9d42090095bf83e0c08c3ead972
AmerBiro <amer.biro@gmail.com>
2022-11-02 11:12:33 +0000

    using props to seperat design from code

--- src/_home/Home.js
+++ src/_home/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   {
@@ -36,17 +37,14 @@ const Home = () => {
      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
 
+  const noBlogs = blogs.length
   return (
+
     <div className="home">
       <h2>Home Page</h2>
-     
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>written by {blog.author}</p>
-        </div>
-
-      ))}
+     <BlogList blogs={blogs} 
+     title={"Number of blogs found " + noBlogs} 
+     end={"Next page"}/>
 
       {/*         <button onClick={clickButton}>Click me</button>
 
