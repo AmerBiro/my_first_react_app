1b901ff8583fbcf4f824798d104fca563a01b268
AmerBiro <amer.biro@gmail.com>
2022-11-03 12:57:32 +0000

    handling submit blog

--- src/_create/Create.js
+++ src/_create/Create.js
@@ -1,9 +1,47 @@
+import { useState } from "react";
+
 const Create = () => {
-     return ( 
-          <div>
-               <h2>Create a new blog</h2>
-          </div>
-      );
-}
- 
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = (e) => {
+     e.preventDefault()
+     const blog = {title, body, author}
+     console.log(blog)
+  }
+
+  return (
+    <div className="create">
+      <h2>Create a new blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label>Blog author</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="author1">Author 1</option>
+          <option value="author2">Author 2</option>
+        </select>
+        <button>Add blog</button>
+      </form>
+    </div>
+  );
+};
+
 export default Create;
